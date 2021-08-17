6835ea70ab3cb7c1370907f15bbe0e4801643ca4
Julia Cassemiro <jg-cassemiro@hotmail.com>
2021-08-17 14:43:03 -0300

    formulario recebe prop com titulo/nota

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
+
 class App extends Component {
+	criarNota(titulo, texto) { //preciso salvar esse titulo e essa nota
+		console.log("uma nova nota foi criada " + titulo + " " + texto);
+	}
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro  criarNota={this.criarNota} />
+        <ListaDeNotas /> //agora a minha lista de notas, precisa receber esse titulo e essa nota, e pra isso acontecer eu preciso passar uma propriedade tbm
       </section>
     );
   }
