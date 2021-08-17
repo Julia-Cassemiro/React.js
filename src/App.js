2ed0f2d3e5a9da284c106eec9849a5bd93fb6635
Julia <julia@garimpo.io>
2021-08-17 19:23:33 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -5,24 +5,35 @@ import "./assets/App.css";
 import './assets/index.css';
 
 class App extends Component {
+	//passo um array vazio de notas
 	constructor() {
 		super();
-		this.notas = [];
+		this.state = {
+			notas: []
+		};
 	}
 
 	criarNota(titulo, texto) { //preciso salvar esse titulo e essa nota
-		const novaNota = {titulo, texto};
-		this.notas.push(novaNota);
-		console.log(this.notas.length);
+		//eu crio o objeto
+		const novaNota = { titulo, texto };
+		//criar um novo array
+		const novoArrayNotas = [...this.state.notas, novaNota];
+		//passo um novo state pra ele
+		const novoEstado = {
+			notas:novoArrayNotas
+		};
+		//ai seto esse state
+		this.setState(novoEstado);
 	}
-  render() {
-    return (
-      <section className="conteudo">
-        <FormularioCadastro  criarNota={this.criarNota.bind(this)} />
-        <ListaDeNotas notas={this.notas} />  
-      </section>
-    );
-  }
+	render() {
+		return (
+			<section className="conteudo">
+				<FormularioCadastro criarNota={this.criarNota.bind(this)} />
+				<ListaDeNotas notas={this.state.notas} />
+			</section>
+		);
+	}
 }
+
 //minha lista de notas, precisa receber esse titulo e essa nota, e pra isso acontecer eu preciso passar uma propriedade tbm
 export default App;
